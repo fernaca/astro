4248ea6e7bba87423867e4ce4ecd1d1d798d8496
Fernando Caceres <fernando.caceres@gmail.com>
2022-07-06 10:17:12 -0300

    Variables en ambiente

--- astro/settings.py
+++ astro/settings.py
@@ -1,23 +1,21 @@
 import os
-from decouple import config
-from decouple import config, Csv
 from pathlib import Path
-
+import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-
+env = environ.Env()
+environ.Env.read_env()
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+SECRET_KEY = env('SECRET_KEY') 
+DEBUG = env('DEBUG') 
 #ALLOWED_HOSTS =  []# ['127.0.0.1']  #
-# Application definition
+ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(' ')
 
+# Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
